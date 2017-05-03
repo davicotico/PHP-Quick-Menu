69dae1b95fe6f477de9bee8bf7330f68a5f85a82
David Ticona Saravia <david.ticona.saravia@gmail.com>
2017-05-02 23:02:52 -0300

    docs

--- BootstrapMenu.php
+++ BootstrapMenu.php
@@ -5,12 +5,12 @@ require 'QuickMenu.php';
 class BootstrapMenu extends QuickMenu
 {
     public function __construct($options = array())
-	{
+    {
         parent::__construct($options);
         $this->set('dropdownIcon', '<i class="caret"></i>');
         $this->set('ul-root', array('class'=>'nav navbar-nav', 'id'=>'#myMenu'));
         $this->set('ul', array('class'=>'dropdown-menu'));
         $this->set('li-parent', array('class'=>'dropdown'));
         $this->set('a-parent', array('class'=>"dropdown-toggle", 'data-toggle'=>"dropdown", 'role'=>"button", 'aria-haspopup'=>"true", 'aria-expanded'=>"false"));
-	}
+    }
 }
